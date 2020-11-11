c0f208336bd5358eec017f3879b478cbd00c16a7
Tiago Sales <tiagoasales@gmail.com>
2020-11-11 11:22:52 -0300

    Desafio ReactJS da Rocketseat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect( () => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+}, []);
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('repositories', {
+      title: `Novo Repositório ${Date.now()}`,
+      url: "http://github.com/tiagoasales",
+      techs: ['Devops', 'Helm'],
+      likes: 0
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    
+    const response = await api.delete(`repositories/${id}`);    
+    
+    if (response.status === 204) {
+      const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+      const repositoriesUpdated = [...repositories];
+      repositoriesUpdated.splice(repositoryIndex, 1);
+      setRepositories(repositoriesUpdated);
+      console.log("deleted");
+    }
+     
+    console.log(id);
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid="repository-list">    
+        {repositories.map(repository => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          );
+        })}      
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
